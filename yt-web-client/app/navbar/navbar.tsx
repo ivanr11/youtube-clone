2269e9eed3bba32ab78f0ce2f281d217e838d403
Ivan Rojas <184444587+ivanr11@users.noreply.github.com>
2024-10-11 15:00:50 -0400

    Add sign in with Google

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,26 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+    // Init user state
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // Clean subscription on unmount
+        return () => unsubscribe();
+    });
+
     return(
         <nav className={styles.nav}>
             <Link href="/">
@@ -13,6 +31,7 @@ export default function Navbar() {
                     alt="YouTube Logo"
                 />
             </Link>
+            <SignIn user={user}/>
         </nav>
     );
 }
